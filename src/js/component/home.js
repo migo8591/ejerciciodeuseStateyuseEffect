1248a233e547f74020fc08157db1a0c39ebf4019
migo8591 <mcatedral24@hotmail.com>
2021-03-20 11:50:37 +0000

    ejercicio uuseState y useEffect

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 export function Home() {
+	const [count, setCount] = useState(0);
+	const [inputValue, setInputValue] = useState("Miguel");
+
+	////////////////////////////////////////
+	useEffect(() => {
+		console.log("count: ", count);
+		console.log("input value: ", inputValue);
+	}, []);
+
+	/////////////////////////////////////
+	const handleClick = () => {
+		setCount(count + 1);
+	};
+	const handleChange = e => {
+		setInputValue(e.target.value);
+	};
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>You clicked {count} times!</h1>
+			<h1>Hello {inputValue} from CR!</h1>
+
 			<p>
 				<img src={rigoImage} />
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<button className="btn btn-primary" onClick={handleClick}>
+				Click here
+			</button>
+
+			<input onChange={event => handleChange(event)} />
 		</div>
 	);
 }
